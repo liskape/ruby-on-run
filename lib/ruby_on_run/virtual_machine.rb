ef3b4df678c2d4789faed7d75de10a6a952f06da
Ondřej Kála <kala.ondrej@gmail.com>
2014-10-30 13:41:08 +0100

    more instruction methods

--- lib/ruby_on_run/virtual_machine.rb
+++ lib/ruby_on_run/virtual_machine.rb
@@ -12,7 +12,7 @@ class RubyOnRun::VirtualMachine
     # interpret instructions
   end
   
-  def noop
+  def noop(args)
   
   end
   
@@ -61,6 +61,16 @@ class RubyOnRun::VirtualMachine
     @current_stack_frame.bytecode_pointer = args[0] if !top.nil? && top
   end
   
+  def ret(args)
+    top = @current_stack_frame.pop
+	if !@current_stack_frame.parent.nil?	
+      @current_stack_frame.parent.push(top)
+	  @current_stack_frame = @current_stack_frame.parent
+	else
+	  # TODO
+	end
+  end
+  
   def swap_stack(args)
     top1 = @current_stack_frame.pop
 	top2 = @current_stack_frame.pop
@@ -81,8 +91,109 @@ class RubyOnRun::VirtualMachine
 	top_x.each { |x| @current_stack_frame.push(x) }
   end
   
-  def pop()
+  def pop(args)
     @current_stack_frame.pop
   end
+  
+  def pop_many(args)
+    args[0].times @current_stack_frame.pop
+  end
+  
+  def rotate(args)
+    top_x = []
+	args[0].times top_x.push(@current_stack_frame.pop)
+    top_x.each { |x| @current_stack_frame.push(x) }	
+  end
+  
+  def move_down(args)
+    top = @current_stack_frame.pop
+	top_x = []
+	args[0].times top_x.push(@current_stack_frame.pop)
+    @current_stack_frame.push(top)
+	top_x.reverse!
+    top_x.each { |x| @current_stack_frame.push(x) } 	
+  end
+  
+  def set_local(args)
+    
+  end	
+  
+  def push_local(args)
+    
+  end	
+  
+  def push_local_depth(args)
+    
+  end	
+
+  def set_local_depth(args)
+  
+  end
+  
+  def passed_arg(args)
+  
+  end
+  
+  def push_current_exception(args)
+  
+  end
+  
+  def clear_exception(args)
+  
+  end
+  
+  def push_exception_state(args)
+  
+  end
+  
+  def restore_exception_state(args)
+  
+  end
+  
+  def raise_exc(args)
+  
+  end
+  
+  def setup_unwind(args)
+  
+  end
+  
+  def pop_unwind(args)
+  
+  end
+  
+  def raise_return(args)
+  
+  end
+  
+  def ensure_return(args)
+  
+  end
+  
+  def raise_break(args)
+  
+  end
+  
+  def reraise(args)
+  
+  end
+  
+  def make_array(args)
+    a = []
+	args[0].times a.push(@current_stack_frame.pop)
+	a.reverse!
+	@current_stack_frame.push(a)
+  end
+  
+  def cast_array(args)
+    # TODO
+  end
+  
+  def shift_array(args)
+    a = @current_stack_frame.pop
+	first = a.shift
+	@current_stack_frame.push(a)
+	@current_stack_frame.push(first)	
+  end 
   
 end
