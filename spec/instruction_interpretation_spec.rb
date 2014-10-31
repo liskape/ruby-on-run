b54e04771677248a88ca8433712295a094413f25
Ondrej Kala <kala.ondrej@gmail.com>
2014-10-31 14:51:25 -0400

    add files

---
+++ spec/instruction_interpretation_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require_relative 'spec_helper'
+require_relative '../instruction_interpretation'
+
+describe 'InstructionInterpretation' do
+	
+  subject(:subject) { InstructionInterpretation.new(data) }
+
+end
