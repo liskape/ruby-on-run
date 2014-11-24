fc3611962a111355631697f6ac9929a9ed2f50e8
Ondrej Kala <kala.ondrej@gmail.com>
2014-11-24 10:37:44 -0500

    + interpret method

--- lib/ruby_on_run/virtual_machine.rb
+++ lib/ruby_on_run/virtual_machine.rb
@@ -6,22 +6,25 @@ class RubyOnRun::VirtualMachine
   def initialize(stream)
     @stream = stream
 
-    @code = RubyOnRun::Bytecode.load(@stream).body
-    @current_stack_frame = RubyOnRun::StackFrame.new(@code)
-    @vm_stack = RubyOnRun::Stack.new @current_stack_frame
+    @code = RubyOnRun::Bytecode.load(@stream).body    
     @classes = []
-    # initialize other things ...
-    
+    # initialize other things ...    
   end
 
   def run
+    interpret(@code, nil, nil)
+  end
+
+  def interpret(code, scope, klass)
+    @current_stack_frame = RubyOnRun::StackFrame.new(@code)
+    @vm_stack = RubyOnRun::Stack.new @current_stack_frame
     debug = false
     while @current_stack_frame
       instruction = @current_stack_frame.next_instruction
       instruction.print if debug     
       send instruction.name, instruction.param_hash
       if debug
-        p 'top = ' + @current_stack_frame.top.to_s
+        p 'top = ' + @current_stack_frame.top.to_s if @current_stack_frame.top
         p 'locals = ' + @current_stack_frame.locals.to_s
 		p 'binding = ' + @current_stack_frame.binding.to_s
         p '==========='
