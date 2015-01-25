c3f452dca81f4f2901b5a7afdfe316bec9fdeb40
Ondrej Kala <kala.ondrej@gmail.com>
2015-01-25 04:21:48 -0500

    remove next from block

--- spec/dpll_sat_solver/loader.rb
+++ spec/dpll_sat_solver/loader.rb
@@ -9,10 +9,11 @@ class Loader
     f.each_line do |line|
       t = line.split(" ")
       var_count = t[2].to_i if line.start_with? 'p'
-      next if line.start_with?('p') || line.start_with?('c')
-      clause = []
-      t.each { |x| clause << x.to_i if x != "0\n" }
-      a << clause
+      if !line.start_with?('p') && !line.start_with?('c')
+        clause = []
+        t.each { |x| clause << x.to_i if x != "0\n" }
+        a << clause
+      end
     end
     f.close
     Instance.new(a, var_count)
