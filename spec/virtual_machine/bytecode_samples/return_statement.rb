630ceea48e5f39fd89009956b7687498383db8cf
Petr Liška <liskape8@fit.cvut.cz>
2015-01-25 15:46:15 +0100

    asaf

--- spec/virtual_machine/bytecode_samples/return_statement.rb
+++ spec/virtual_machine/bytecode_samples/return_statement.rb
@@ -1,21 +1,23 @@
-def some_method
-  return 0 if true
-  1
-end
-
-def another_method
-  if true 
-    return 0
+class Dummy
+  def some_method
+    return 0 if true
+    1
   end
-  1
+
+  def another_method
+    if true 
+      return 0
+    end
+    1
+  end
+
+  def another_another_method
+    if true then return 0 end
+    1
+  end
 end
 
-def another_another_method
-  if true then return 0 end
-  1
-end
 
-
-puts some_method
-puts another_method
-puts another_another_method
+puts Dummy.new.some_method
+puts Dummy.new.another_method
+puts Dummy.new.another_another_method
